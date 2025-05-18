f77d0e607086af6110c72d71832dc92ffd67afb8
nidal999212 <ndalmameri2@gmail.com>
2025-05-18 22:22:05 +0100

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,30 @@
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, ContextTypes
+
+TOKEN = '7530759323:AAF7oNowTgos9csp5kgyysQ2FwexkWEIOHs'
+
+# دالة التعامل مع /start
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    keyboard = [
+        [InlineKeyboardButton("رابط خداع", callback_data="show_facebook")],
+        [InlineKeyboardButton("الذهاب إلى بوتي الثاني", url="https://t.me/Hgi6_bot")]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await update.message.reply_text("اختر أحد الخيارات:", reply_markup=reply_markup)
+
+# دالة التعامل مع الضغط على زر
+async def handle_button(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    await query.answer()
+
+    if query.data == "show_facebook":
+        await query.message.reply_text("هذا رابط ارسله للضحية وانضر نتيجة حتى تصلك:\nhttps://facebook.com")
+
+if __name__ == '__main__':
+    app = ApplicationBuilder().token(TOKEN).build()
+
+    app.add_handler(CommandHandler('start', start))
+    app.add_handler(CallbackQueryHandler(handle_button))
+
+    print("[*] Bot is running...")
+    app.run_polling()
